4185c98f0bb3f261e0b731cce845b0c11ecd473c
Alex <iordan.alexandru97@yahoo.com>
2022-06-29 10:57:44 +0200

    Working updated project

--- countries_discrepancy.py
+++ countries_discrepancy.py
@@ -1,4 +1,5 @@
-from numpy import full
+from mimetypes import init
+from numpy import place
 import tweepy
 from googletrans import Translator
 import pandas as pd
@@ -6,29 +7,32 @@ import re
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from api_file import apiKey, apiKeySecret, accessToken, accessTokenSecret
 
 translator = Translator()
-print(translator.translate("안녕하세요.", dest="en").text)
-
-apiKey = "jyVEAnlDY1lBPycr8HUNqaWxS"
-apiKeySecret = "gScJ1A2l5mCkOrAF37BxLT9oqm7ij6hNNdZLGTaB9gyTvB9GVV"
-bearerToken = "AAAAAAAAAAAAAAAAAAAAAKmTeAEAAAAAwAEd0y2qzGa7eBU4bKI%2FJp07yTo%3DkG8KHKQ083AOjeOKxA1MlrZs34usHDmp92Hs3iexv5LxRHJH5O"
-accessToken = "1496904203784048640-lYP2vvV1LiBKxqAjNz24ppYKWmnDEG"
-accessTokenSecret = "NpDlvtPfdr7pOzdI627fbs9BtBEFKL9jm3HEWCNw9RjQJ"
 
 auth = tweepy.OAuthHandler(apiKey, apiKeySecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
-countries = ["Germany"]
 
-for country in countries:
+
+def getTweetsByCountry(country, n=1000):
     places = api.search_geo(query=country, granularity="country")
-    # print(places)
     place_id = places[0].id
-    # tweets = api.search_tweets(q="place:%s" % place_id, count=3, result_type="popular")
 
+    results = [
+        status._json
+        for status in tweepy.Cursor(
+            api.search_tweets,
+            q="place:%s" % place_id,
+            tweet_mode="extended",
+        ).items(n)
+    ]
+    return results
+
+
+def processData(results):
     dictionar = {
         "text": [],
         "likes": [],
@@ -37,19 +41,6 @@ for country in countries:
         "sentiment_analysis": [],
         "url": [],
     }
-
-    results = [
-        status._json
-        for status in tweepy.Cursor(
-            api.search_tweets,
-            q="place:%s" % place_id,
-            # result_type="popular",
-            tweet_mode="extended",
-        ).items(10000)
-    ]
-    info = []
-    print("len results: ", len(results))
-
     sia = SentimentIntensityAnalyzer()
     for result in results:
         full_text = translator.translate(result["full_text"], dest="en").text
@@ -65,78 +56,18 @@ for country in countries:
             dictionar["url"].append(result.group(1))
         else:
             dictionar["url"].append("")
-
-    # print(dictionar)
-    df = pd.DataFrame(dictionar)
-    print(df)
-    df.to_csv("information_" + country + "_extended.csv")
-    df.to_excel("information_" + country + "_extended.xlsx", index=False)
-# for result in results:
-
-#     tweet = {
-#         "id": result["id"],
-#         "tweet text": result["full_text"],
-#         "follower count": result["user"]["followers_count"],
-#         "likes count": result["user"]["favourites_count"],
-#         "city": result["place"]["name"],
-#     }
-#     info.append(tweet)
-
-# from collections import ChainMap
-
-# d3 = ChainMap(*info)
-# new_dict = dict(d3)
-# print("new dict\n", new_dict)
-
-# df = pd.DataFrame(
-#     new_dict.items(),
-#     columns=[new_dict["id"], new_dict["tweet text"]],
-# )
-# print("Dataframe\n", df)
-# df.to_csv("information.csv", sep="\t")
+    return dictionar
 
 
-# sia = SentimentIntensityAnalyzer()
-# df["sentiment analysis"] = sia.polarity_scores(df["tweet text"])["compound"]
-# print(df)
+def exportToFile(country, df, tag):
+    df.to_csv("datasets/information_" + country + "_" + tag + ".csv")
+    df.to_excel("datasets/information_" + country + "_" + tag + ".xlsx", index=False)
 
 
-# print(tweets)
-# for tweet in tweets:
-#     print(translator.translate(tweet.text, dest="en").text + " | " + tweet.place.name)
-#     # status = api.get_status(tweet.id_str, tweet_mode="extended")
-#     # print(status.extended_tweet["full_text"])
-#     print("!!!!!!!!")
-#     print(tweet.entities["hashtags"])
-
-
-# def get_trends(api, loc):
-#     # Object that has location's latitude and longitude.
-#     g = geocoder.osm(loc)
-
-#     closest_loc = api.closest_trends(g.lat, g.lng)
-#     trends = api.get_place_trends(closest_loc[0]["woeid"])
-#     return trends[0]["trends"]
-
-
-# def extract_hashtags(trends):
-#     hashtags = [trend["name"] for trend in trends]
-#     # if "#" in trend["name"]
-#     return hashtags
-
-
-# def get_n_tweets(api, hashtag, n, lang=None):
-#     for status in tweepy.Cursor(api.search_tweets, q=hashtag, lang=lang).items(n):
-#         print(f"https://twitter.com/i/web/status/{status.id}")
-
-
-# if __name__ == "__main__":
-#     loc = "UK"
-#     trends = get_trends(api, loc)
-#     hashtags = extract_hashtags(trends)
-#     for hashtag in hashtags:
-#         print(translator.translate(hashtag, dest="en").text)
-#     hashtag = hashtags[0]
-#     print("!!!!!!")
-#     print(hashtag)
-#     status = get_n_tweets(api, hashtag, 5, "ar")
+if __name__ == "__main__":
+    countries = ["Germany", "Norway", "Romania", "Switzerland"]
+    for country in countries:
+        tweets = getTweetsByCountry(country, 10)
+        tweetsDictionary = processData(tweets)
+        df = pd.DataFrame(tweetsDictionary)
+        exportToFile(country, df, "simple")
